c88c2ad33778aa2a47bad937056fe17b7c2fc0ea
Anna Hilda <anna.frezarini@aluno.senai.br>
2026-08-07 19:16:51 -0300

    Update alerta.dart

--- lib/TCC/models/alerta.dart
+++ lib/TCC/models/alerta.dart
@@ -29,4 +29,21 @@ class Alerta {
     this.resolvido = false,
     required this.criadoEm,
   });
+
+  factory Alerta.fromJson(Map<String, dynamic> json) {
+    return Alerta(
+      id: int.parse(json['id'].toString()),
+      sensorTipo: TipoSensor.porChave(json['sensor_tipo'] as String),
+      severidade: Severidade.values.firstWhere(
+        (s) => s.chave == json['severidade'],
+        orElse: () => Severidade.info,
+      ),
+      mensagem: json['mensagem'] as String,
+      valor: double.tryParse(json['valor']?.toString() ?? '') ?? 0,
+      resolvido: json['resolvido'] == true ||
+          json['resolvido'] == 1 ||
+          json['resolvido'] == '1',
+      criadoEm: DateTime.parse(json['criado_em'] as String),
+    );
+  }
 }
